2b6f723e57e04c795f12439f7e7040649dce5184
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-01-23 23:37:22 +0530

    first commit

---
+++ src/main/java/com/vishnu/unsplash/UnsplashApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.unsplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashApplication.class, args);
+	}
+
+}
